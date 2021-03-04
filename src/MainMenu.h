6361f20001f151e0baa18051727ac7ee51d6af43
min <seetminyi80@gmail.com>
2021-03-04 13:26:05 +0800

    MainMenu

--- src/MainMenu.h
+++ src/MainMenu.h
@@ -1,12 +1,10 @@
 #pragma once
 namespace MainMenu
 {
-	//Level main entry points
+
 	void Init(void);
 	void Update(void);
 	void Exit(void);
-
-	// Collection of drawing related code.
 
 	void Load(void);
 	void Unload(void);
