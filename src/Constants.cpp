302ce6cc6a567123eccdeee87f2f6c92c5ce13e2
min <seetminyi80@gmail.com>
2021-03-09 02:40:29 +0800

    Added Heart Shape Sprite

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -19,6 +19,7 @@ const char* FontFile = "../Assets/Font/Roboto-Regular.ttf";
 const char* ButtonTest = "../Assets/Art/Button.png";
 const char* ButtonTest2 = "../Assets/Art/PlanetTexture.png";
 const char* boi = "../Assets/Art/boi.png";
+const char* Heart = "../Assets/Art/PlanetTexture.png";
 
 // Player constants
 const float player_speed = 1.5f;
