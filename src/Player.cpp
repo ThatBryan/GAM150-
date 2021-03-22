3b7dd412ea6fa01cf44d4bfbfa397c01d81c6278
min <seetminyi80@gmail.com>
2021-03-23 02:52:28 +0800

    charged jump

--- src/Player.cpp
+++ src/Player.cpp
@@ -18,8 +18,8 @@ extern std::array <AudioClass, static_cast<int>(AudioID::Max)> soundTest;
 extern LevelSystem LevelSys;
 
 Player::Player(AEGfxTexture* texture, const f32 width, const f32 height) : sprite(texture, width, height), lose{false},
-active{ true }, gravity{ false }, jump{ false }, win{ false }, startingPos{ 0, 0 }, vel{ 0, 0 }, jumpvel{player_jumpvel},
-hp(), direction{MovementState::Right}
+active{ true }, gravity{ false }, jump{ false }, chargedjump{ false }, win{ false }, startingPos{ 0, 0 }, vel{ 0, 0 }, jumpvel{ player_jumpvel },
+hp(), direction{MovementState::Right}, chargedjumpvel{ player_chargedjumpvel }
 {
 	playerBB.color.Set(Color{ 0, 0, 0, 255.0f });
 	bottomBB.color.Set(Color{ 255.0f, 255.0f, 0, 255.0f }); // yellow
@@ -32,8 +32,8 @@ hp(), direction{MovementState::Right}
 	hp.current = player_hp_max;
 }
 
-Player::Player() : lose{ false }, active{ true }, gravity{ false }, jump{ false },
-win{ false }, startingPos{ 0, 0 }, vel{ 0, 0 }, jumpvel{ player_jumpvel }, 
+Player::Player() : lose{ false }, active{ true }, gravity{ false }, jump{ false }, chargedjump { false },
+win{ false }, startingPos{ 0, 0 }, vel{ 0, 0 }, jumpvel{ player_jumpvel }, chargedjumpvel{ player_chargedjumpvel },
 hp(), direction{ MovementState::Right } {
 
 	playerBB.color.Set(Color{ 0, 0, 0, 255.0f });
@@ -50,11 +50,13 @@ hp(), direction{ MovementState::Right } {
 void Player::Reset(void)
 {
 	jump = false;
+	chargedjump = false;
 	win = false;
 	lose = false;
 	active = true;
 	sprite.pos = startingPos;
 	jumpvel = player_jumpvel;
+	chargedjumpvel = player_chargedjumpvel;
 	hp.current = hp.max;
 	sprite.rotation = 0;
 	direction = MovementState::Right;
@@ -94,12 +96,13 @@ void Player::Update_Position(void)
 	if (!jump && !gravity && (AEInputCheckTriggered(AEVK_W) || AEInputCheckTriggered(AEVK_UP)))
 	{
 		if (!DebugMode) {
-			jump = TRUE;
+			jump = true;
 			Audio.playAudio(soundTest[static_cast<int>(AudioID::Jump)], AudioID::Jump);
 		}
 	}
 	if (jump)
 	{
+		printf("jump\n");
 		if (sprite.pos.y + sprite.height / 2 <= maxY)
 		{
 			sprite.pos.y -= jumpvel;
@@ -113,9 +116,44 @@ void Player::Update_Position(void)
 		}
 	}
 
+	static float chargedjump_counter = 1.0f;
+	if (AEInputCheckCurr(AEVK_SPACE) && !chargedjump && !gravity)
+	{
+		chargedjump_counter -= g_dt;
+		if (chargedjump_counter < 0)
+		{
+			chargedjump = true;
+			jump = false;
+
+		}
+		
+		printf("%2f\n", chargedjump_counter);
+	}
+	if (AEInputCheckReleased(AEVK_SPACE))
+	{
+		chargedjump = false;
+		chargedjump_counter = 1.0f;
+	}
+	if (chargedjump)
+	{
+		printf("chargedjump\n");
+		if (sprite.pos.y + sprite.height / 2 <= maxY)
+		{
+			sprite.pos.y -= chargedjumpvel;
+
+			chargedjumpvel -= 0.2f; // velocity decrease as y increases
+			if (chargedjumpvel < -10.0f)
+			{
+				chargedjump = false;
+				chargedjumpvel = 10.0f;
+			}
+		}
+	}
+
 	if (!gravity) // reset counter if player's feet touches the ground
 	{
 		jumpvel = 5.0f;
+		chargedjumpvel = 10.0f;
 	}
 
 	if (AEInputCheckCurr(AEVK_D) || AEInputCheckCurr(AEVK_RIGHT))
@@ -182,7 +220,7 @@ void Player::CheckOutOfBound() {
 
 void Player::GravityManager(void)
 {
-	if (gravity && !jump)
+	if (gravity && !jump && !chargedjump)
 	{
 		if(!DebugMode)
 			sprite.pos.y += gravityStrength * g_dt;
