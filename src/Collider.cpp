74bf388709162c9effc1f344df6094229840cf68
Bryan <usernamee19@gmail.com>
2021-03-24 18:43:39 +0800

    ok

--- src/Collider.cpp
+++ src/Collider.cpp
@@ -5,9 +5,14 @@ Collider::Collider() : topBB{ 0.0f, 0.0f }, bottomBB{ 0.0f, 0.0f }, leftBB{ 0.0f
 
 
 void Collider::SetWidthHeight(Graphics::Rect& arg, float width, float height) {
-	std::cout << "Wtf\n";
 	arg.width = width;
 	arg.height = height;
+
+
+	bottomBB.color.Set(Color{ 255.0f, 255.0f, 0, 255.0f }); // yellow
+	topBB.color.Set(Color{ 255.0f, 0, 0, 255.0f }); // red
+	leftBB.color.Set(Color{ 0, 255.0f, 0, 255.0f }); // green
+	rightBB.color.Set(Color{ 0, 0, 255.0f, 255.0f }); // blue
 }
 
 void Collider::Draw()
